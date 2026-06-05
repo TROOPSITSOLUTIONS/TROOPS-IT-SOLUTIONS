7cc6c220d860f4d0d6400eb8a074e21cbefe83a9
TROOPS IT SOLUTIONS <troopsitsolutions@gmail.com>
2026-06-05 12:22:40 +0600

    Add files via upload

---
+++ teambct-config-server.js
@@ -0,0 +1,6 @@
+// ========================================================
+// TEAM BCT GLOBAL ENGINE CONFIGURATION
+// ========================================================
+const GIST_OWNER = 'TROOPSITSOLUTIONS';
+const GIST_ID = '3bfa4b9ca8dbdc1038918d63329096b3';
+const GIST_URL = `https://gist.githubusercontent.com/${GIST_OWNER}/${GIST_ID}/raw/tools.json`;
